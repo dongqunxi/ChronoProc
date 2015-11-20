be8b00b21e937921856e03e89f7023e37e2b7628
dongqunxi <dongqunxi@gmail.com>
2015-11-20 12:28:34 +0800

    adjusted threshold k=20, radius=11

--- Terminal_Strategy/stren_ROIs.py
+++ Terminal_Strategy/stren_ROIs.py
@@ -95,14 +95,13 @@ def apply_norm(fn_stc, event, ref_event, thr=95):
         stcs.append(stc)
         fn_nr = fname[:fname.rfind('-lh')] + '_norm_1'
         stc.save(fn_nr, ftype='stc')
-    stcs_path = subjects_dir+'/fsaverage/dSPM_ROIs/conditions/' 
-    reset_directory(stcs_path)
+    stcs_path = subjects_dir+'/fsaverage/dSPM_ROIs/' 
     fn_avg = stcs_path + '%s' %(event)
     stcs = np.array(stcs)
     stc_avg = np.sum(stcs, axis=0)/stcs.shape[0]
     stc_avg.save(fn_avg, ftype='stc')
 
-def apply_rois(fn_stc_list, event, min_subject='fsaverage', radius=8., tmin=0.0, tmax=0.3, thr=95):
+def apply_rois(fn_stc_list, event, min_subject='fsaverage', k=15, radius=20., tmin=0.0, tmax=0.3, thr=95):
     """
     Compute regions of interest (ROI) based on events
     ----------
@@ -140,7 +139,7 @@ def apply_rois(fn_stc_list, event, min_subject='fsaverage', radius=8., tmin=0.0,
         cal_mean=stc_intr.data.mean(axis=-1)
         non_data=stc_intr.data[cal_mean>0]
         inds = np.argwhere(cal_mean)
-        pca = PCA(n_components=8).fit(non_data)
+        pca = PCA(n_components=k).fit(non_data)
         kmeans = KMeans(init=pca.components_, n_clusters=8)
         kmeans.fit(non_data)
         codebook = kmeans.cluster_centers_
@@ -270,7 +269,7 @@ def apply_comSTC(stcs_path, evt_list):
     import glob
     stcs = []
     for evt in evt_list:
-        fname = stcs_path + evt
+        fname = stcs_path + '%s-lh.stc' %evt
         stc = mne.read_source_estimate(fname)
         stcs.append(stc) 
     stcs = np.array(stcs)
@@ -278,7 +277,7 @@ def apply_comSTC(stcs_path, evt_list):
     fn_avg = subjects_dir+'/fsaverage/dSPM_ROIs/common' 
     stc_avg.save(fn_avg, ftype='stc')
 
-def apply_stand(fn_stc, radius=5.0, min_subject='fsaverage'):
+def apply_stand(fn_stc, radius=10.0, min_subject='fsaverage'):
 
     """
     ----------
@@ -321,3 +320,132 @@ def apply_stand(fn_stc, radius=5.0, min_subject='fsaverage'):
                                                  n_jobs=1)
                     func_label = func_label[0]
                     func_label.save(stan_path + '%s' %f)
+def _sortlist(label_list, stc, src):
+    sort_list = []
+    sort_list.append(label_list[0])
+    for test_fn in label_list[1:]:
+        test_label = mne.read_label(test_fn)
+        i = 0
+        insert = False
+        while (i < len(sort_list)) and insert == False:
+            class_label = mne.read_label(sort_list[i])
+            class_pca = stc.extract_label_time_course(class_label, src, mode='pca_flip')
+            test_pca = stc.extract_label_time_course(test_label, src, mode='pca_flip')
+            class_pca = np.squeeze(class_pca)
+            test_pca = np.squeeze(test_pca)
+            class_pow = np.sum(class_pca ** 2)
+            test_pow = np.sum(test_pca ** 2)
+            # sort the list
+            if test_pow < class_pow:
+                sort_list.insert(i, test_fn)
+                insert = True
+            i = i + 1
+             
+        if insert == False:
+            sort_list.append(test_fn)
+       
+    return sort_list
+    
+def _cluster_rois(sel_path, label_list, stc, src, min_dist, mni_subject='fsaverage'):
+    """
+    subfunctions of merge_ROIs
+    ----------
+    mer_path: str
+        The directory for storing merged ROIs.
+    label_list: list
+        Labels to be merged
+    """
+    class_list = []
+    label_list = _sortlist(label_list, stc, src)
+    class_list.append(label_list[0])
+    for test_fn in label_list[1:]:
+        test_label = mne.read_label(test_fn)
+        i = 0
+        belong = False
+        while (i < len(class_list)) and (belong is False):
+            class_label = mne.read_label(class_list[i])
+            if test_label.hemi != class_label.hemi:
+                i = i + 1
+                continue
+            else:
+                # Get the centroids
+                class_stc = stc.in_label(class_label)
+                test_stc = stc.in_label(test_label)
+                class_pca = stc.extract_label_time_course(class_label, src, mode='pca_flip')
+                test_pca = stc.extract_label_time_course(test_label, src, mode='pca_flip')
+                class_pca = np.squeeze(class_pca)
+                test_pca = np.squeeze(test_pca)
+                class_pow = np.sum(class_pca ** 2)
+                test_pow = np.sum(test_pca ** 2)
+                
+                if class_label.hemi == 'lh':
+                    h = 0
+                elif class_label.hemi == 'rh':
+                    h = 1
+                class_seed, _, _ = class_stc.center_of_mass(mni_subject, hemi=h)
+                test_seed, _, _ = test_stc.center_of_mass(mni_subject, hemi=h)
+                class_mni = mne.vertex_to_mni(class_seed, h, mni_subject)[0]
+                test_mni = mne.vertex_to_mni(test_seed, h, mni_subject)[0]
+                
+                exch = False
+                if np.max(class_pow) < np.max(test_pow):
+                    exch = True
+                
+                # Compute the centroids distance   
+                if np.linalg.norm(class_mni - test_mni) < min_dist:
+                    if exch == True:
+                        os.remove(class_list[i])
+                        class_list[i] = test_fn
+                        class_list = _sortlist(class_list, stc, src)
+                    elif exch == False:
+                        os.remove(test_fn)
+                    belong = True
+                i = i + 1
+                
+        if belong is False:
+            class_list.append(test_fn)
+            class_list = _sortlist(class_list, stc, src)
+                
+    return len(class_list)
+
+
+def apply_sele(fn_stc_list, fn_src, min_dist):
+    """
+    select ROIs, so that the overlapped lables merged into one. 
+    If 'group' is False, ROIs from all the events are merged and 
+    saved in the folder 'ROIs' under the 'labels_path'.
+    If 'group' is True, ROIs from all the subjects are merged and
+    saved in the folder 'merged' under the 'labels_path'.
+    ----------
+    labels_path: the total path of all the ROIs' folders.
+    group: if 'group' is False, merge ROIs from different events within one
+           subject, if 'group' is True, merge ROIs across subjects.
+    evelist: events name of all subfolders
+    """
+    fn_stc_list = get_files_from_list(fn_stc_list)
+    # loop across all filenames
+    for fn_stc in fn_stc_list:
+        import glob, shutil
+        labels_path = os.path.split(fn_stc)[0]
+        sel_path = labels_path + '/ROIs/sele/' 
+        reset_directory(sel_path)
+        source_path = labels_path + '/ROIs/stand/' 
+        source = glob.glob(os.path.join(source_path, '*.*'))
+        for filename in source:
+            shutil.copy(filename, sel_path) 
+        reducer = True
+        stc = mne.read_source_estimate(fn_stc)
+        stc.data[stc.data < 0] = 0
+        #stc = stc.crop(tmin, tmax)
+        src = mne.read_source_spaces(fn_src)
+        while reducer:
+            list_dirs = os.walk(sel_path)
+            label_list = ['']
+            for root, dirs, files in list_dirs:
+                for f in files:
+                    label_fname = os.path.join(root, f)
+                    label_list.append(label_fname)
+            label_list = label_list[1:]
+            len_class = _cluster_rois(sel_path, label_list, stc, src, min_dist)
+            if len_class == len(label_list):
+                reducer = False  
